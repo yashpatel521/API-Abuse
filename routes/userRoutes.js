bf0acc98297a43bbf8ce12a46960b97b7cf62b8e
Yash Patel <yash1451999@gmail.com>
2023-07-04 21:12:16 -0400

    SQL injection/ Brute force html

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,24 +3,29 @@ const conn = require("../config");
 
 const router = express.Router();
 
+//api for login
 router.route("/login").post((req, res) => {
   const { email, password } = req.body;
 
   const selectQuery = `SELECT * FROM user WHERE email='${email}' AND password='${password}'`;
-
+  console.log(selectQuery);
   conn.query(selectQuery, function (err, result, fields) {
     if (err) res.status(400).send(err);
     if (result.length == 0) {
-      res.status(400).json({
+      res.status(200).json({
+        status: false,
         message: "Invalid credentials",
+        query: selectQuery,
       });
     } else {
       res.status(200).json({
         ...result[0],
+        status: true,
         message: "login Successfull",
+        query: selectQuery,
       });
     }
   });
-}); //api for login
+});
 
 module.exports = router;
